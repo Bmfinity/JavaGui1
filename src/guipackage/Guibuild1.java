b5527ffbe3d78c5c40bd13e87209d8fe1e345dff
Bhargav Premchand Mahajan <bhargavmahajan401@gmail.com>
2021-09-09 15:11:57 +0530

    first commit

---
+++ src/guipackage/Guibuild1.java
@@ -0,0 +1,43 @@
+package guipackage;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Guibuild1 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Guibuild1 window = new Guibuild1();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Guibuild1() {
+		initialize();
+	}
+
+	/**   cghghhf
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
